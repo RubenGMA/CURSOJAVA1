ef4ba2a9b70f5c567b561cd85b8f05061057199e
Ruben <r.gonma@gmail.com>
2022-10-06 13:49:01 +0200

    Maquina expendedora

--- Proy004_MaquinaExpendedora/src/main/java/com/expendedora/domain/Maquina.java
+++ Proy004_MaquinaExpendedora/src/main/java/com/expendedora/domain/Maquina.java
@@ -1,17 +1,49 @@
 package com.expendedora.domain;
 
+/**
+ * Maquina de refrescos con capacidad de 5 tipos de refresco diferente y 100 centimos en cambios
+ * @author ALUMNO
+ *
+ */
 public class Maquina {
 	//atributos
-	int cantidadRefrescos = 0;
-	int cambios;
-	Refresco refresco;
+	private int cantidadRefrescos = 0;
+	private int cambios;
+	private Refresco[] refrescos;
+	private int capacidad;
 	//constructor
+	public Maquina() {
+		super();
+		this.cambios = 100;
+		this.capacidad = 5;
+		refrescos = new Refresco[this.capacidad];
+		
+	}
+	
 	//metodos
 	public void reponer(Refresco r) {
 		
 	}
+	
 	public void venderRefresco() {
 		
 	}
+
+	public int getCantidadRefrescos() {
+		return cantidadRefrescos;
+	}
+
+	public int getCambios() {
+		return cambios;
+	}
+
+	public Refresco[] getRefrescos() {
+		return refrescos;
+	}
+
+	public int getCapacidad() {
+		return capacidad;
+	}
+	
 	
 }
--- Proy004_MaquinaExpendedora/src/main/java/com/expendedora/domain/Refresco.java
+++ Proy004_MaquinaExpendedora/src/main/java/com/expendedora/domain/Refresco.java
@@ -5,10 +5,41 @@ public class Refresco {
 	String sabor = "";
 	int cl = 170;
 	double precio = 0;
-	boolean agotado = false;
+	boolean agotado;
 	int stock = 0;
 	//constructor
-	
+	public Refresco(String sabor, int cl, double precio, int stock) {
+		super();
+		this.sabor = sabor;
+		this.cl = cl;
+		this.precio = precio;
+		this.stock = stock;
+		if(stock == 0) {
+			this.agotado = true;
+		} else {
+			this.agotado = false;
+		}
+	}
 	//metodos
+	
+	public int getStock() {
+		return stock;
+	}
+	public void setStock(int stock) {
+		this.stock = stock;
+	}
+	public String getSabor() {
+		return sabor;
+	}
+	public int getCl() {
+		return cl;
+	}
+	public double getPrecio() {
+		return precio;
+	}
+	public boolean isAgotado() {
+		return agotado;
+	}
+	
 	
 }
