1d4f210490e9a63aad8b9d66c120df04ce65177c
Ruben <r.gonma@gmail.com>
2022-10-11 12:21:29 +0200

    acer 2

--- Proy001_JavaSE/src/main/java/com/curso/modulo6/equals/EjemploEquals.java
+++ Proy001_JavaSE/src/main/java/com/curso/modulo6/equals/EjemploEquals.java
@@ -3,8 +3,9 @@ package com.curso.modulo6.equals;
 public class EjemploEquals {
 	public static void main(String[] args) {
 		String s = "Hola"; //new String("Hola");
-		String p = new String("Hola");
+		String p = new String("Hola"); //crea nuevo objeto, sin el new no lo crea.
 		System.out.println("p==s " + (p==s));
-		
+		s= s.toUpperCase();
+		System.out.println(s);
 	}
 }
